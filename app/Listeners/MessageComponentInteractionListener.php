5a28ee3020fec9dfc5b5c7766a7c2c5bd5d897aa
Danila Ryndin <rda@maxil.org>
2024-03-03 08:07:49 +0300

    discord bot

---
+++ app/Listeners/MessageComponentInteractionListener.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Log;
+use Nwilging\LaravelDiscordBot\Contracts\Listeners\ApplicationCommandInteractionEventListenerContract;
+use Nwilging\LaravelDiscordBot\Events\ApplicationCommandInteractionEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class MessageComponentInteractionListener implements ShouldQueue, ApplicationCommandInteractionEventListenerContract
+{
+    public function replyContent(ApplicationCommandInteractionEvent $event): ?string
+    {
+        return 'loading';
+    }
+
+    public function behavior(ApplicationCommandInteractionEvent $event): int
+    {
+        return static::REPLY_TO_MESSAGE;
+    }
+
+    public function command(): ?string
+    {
+        return null;
+    }
+
+    public function handle(ApplicationCommandInteractionEvent $event): void
+    {
+        // handle the interaction
+        Log::info('event handle');
+        Log::debug(json_encode($event));
+    }
+}

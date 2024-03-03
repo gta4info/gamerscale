df4a8b072afb5d6b08583c348365b0458f6dd896
Danila Ryndin <rda@maxil.org>
2024-03-03 09:51:31 +0300

    discord bot

--- app/Http/Controllers/DiscordInteractionController.php
+++ app/Http/Controllers/DiscordInteractionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Nwilging\LaravelDiscordBot\Contracts\Services\DiscordInteractionServiceContract;
@@ -18,7 +19,22 @@ class DiscordInteractionController
     public function handleDiscordInteraction(Request $request)
     {
         $response = $this->interactionService->handleInteractionRequest($request);
-        Log::debug($request);
+
+        $member = $request->post('member')['user'];
+        $user = User::firstOrCreate(
+            [
+                'oauth_type' => 'discord',
+                'oauth_id' => $member['id']
+            ],
+            [
+                'oauth_id' => $member['id'],
+                'oauth_type' => 'discord',
+                'password' => encrypt($member['id'])
+            ]
+        );
+
+        Log::info($user);
+
         return response()->json($response->toArray(), $response->getStatus());
     }
 }
